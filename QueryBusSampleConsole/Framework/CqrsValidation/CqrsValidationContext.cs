88167b320ba495f2c225995332fa4fd15e8d436a
ایلماز سورانی <i.sourani@SaghehGroup.net>
2021-10-06 10:44:32 +0330

    Move [CqrsValidation]

---
+++ QueryBusSampleConsole/Framework/CqrsValidation/CqrsValidationContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using QueryBusSampleConsole.Framework.Base;
+
+namespace QueryBusSampleConsole.Framework.CqrsValidation
+{
+    internal class CqrsValidationContext
+    {
+        public static void ValidateQuery<TResponse>(IQuery<TResponse> query)
+        {
+
+            var validationContext = new ValidationContext(query, null, null);
+            var result = new List<ValidationResult>();
+
+            if (Validator.TryValidateObject(query, validationContext, result)) return;
+
+            throw new CqrsValidationException(result);
+        }
+    }
+}
---
+++ QueryBusSampleConsole/Framework/CqrsValidation/CqrsValidationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace QueryBusSampleConsole.Framework.CqrsValidation
+{
+    public sealed class CqrsValidationException : Exception
+    {
+        public CqrsValidationException(IEnumerable<ValidationResult> validationResults)
+        {
+            Errors = validationResults.Select(x => new KeyValuePair<string, string>(x.MemberNames.First(), x.ErrorMessage))
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        public Dictionary<string, string> Errors { get; }
+    }
+}
